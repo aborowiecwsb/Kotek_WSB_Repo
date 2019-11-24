9f79dfc54f26f812986b5afe3184e847ca3e86cf
aborowiecwsb <agnieszka--borowiec@wp.pl>
2019-11-24 10:24:01 +0100

    Adding numbers works now

--- KotekConsole/Program.cs
+++ KotekConsole/Program.cs
@@ -13,14 +13,22 @@ namespace KotekConsole
 
             while (true)
             {
-                int menuOption = Convert.ToInt32(Console.ReadLine());
+                
                 Console.WriteLine("1. Dodaj liczby");
                 Console.WriteLine("4. / liczby");
                 Console.WriteLine("2. Odejmij liczby");
                 Console.WriteLine("5. Wyjscie");
                 Console.WriteLine("3. * liczby");
+                int menuOption = Convert.ToInt32(Console.ReadLine());
+                if (menuOption == 5)
+                    break;
 
                 int a = Convert.ToInt32(Console.ReadLine());
+                int b = Convert.ToInt32(Console.ReadLine());
+                if (menuOption == 1)
+                {
+                    Console.WriteLine(a + b);
+                }
 
 
             }
